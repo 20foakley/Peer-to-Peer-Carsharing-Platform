5347958334598221f018616d5060edb7a2738d6f
20foakley <20foakley@gmail.com>
2024-04-30 09:44:01 -0400

    First commit - setting up a Spring environment, first model, and a service template

---
+++ src/main/java/com/example/carrentalproject/model/Listing.java
@@ -0,0 +1,4 @@
+package com.example.carrentalproject.model;
+
+public class Listing {
+}
---
+++ src/main/java/com/example/carrentalproject/model/User.java
@@ -0,0 +1,122 @@
+package com.example.carrentalproject.model;
+
+public class User {
+    private Long id;
+    private String email;
+    private String username;
+    private String password;
+    private boolean isRenter;
+    private boolean isAdmin;
+    private String streetAddress;
+    private String city;
+    private String stateProvince;
+    private String postalCode;
+    private String country;
+    private Double latitude;
+    private Double longitude;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return password;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.password = password;
+    }
+
+    public boolean isRenter() {
+        return isRenter;
+    }
+
+    public void setRenter(boolean renter) {
+        isRenter = renter;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStateProvince() {
+        return stateProvince;
+    }
+
+    public void setStateProvince(String stateProvince) {
+        this.stateProvince = stateProvince;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
