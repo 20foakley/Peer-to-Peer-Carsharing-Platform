3104d1dcbc8755181eb7c4a8ed899c9965fc43b8
20foakley <20foakley@gmail.com>
2024-05-29 10:14:26 -0400

    Working on models (attributes, constraints) for Users, Vehicles, Rentals, Listings, and Reviews.

--- src/main/java/com/example/carrentalproject/repository/UsersRepository.java
+++ src/main/java/com/example/carrentalproject/repository/UsersRepository.java
@@ -1,5 +1,14 @@
 package com.example.carrentalproject.repository;
 
+import com.example.carrentalproject.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class UsersRepository {
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+    Users findByEmail(String email);
+    Users findByUsername(String username);
+
+
+
 }
--- src/main/java/com/example/carrentalproject/service/UsersService.java
+++ src/main/java/com/example/carrentalproject/service/UsersService.java
@@ -1,7 +1,39 @@
 package com.example.carrentalproject.service;
+
+import com.example.carrentalproject.model.Users;
+import com.example.carrentalproject.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsersService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    public Users saveUser(Users user) {
+        return usersRepository.save(user);
+    }
+
+    public Optional<Users> getUserById(Long id) {
+        return usersRepository.findById(id);
+    }
+
+    public Users getUserByEmail(String email) {
+        return usersRepository.findByEmail(email);
+    }
+
+    public List<Users> getAllUsers() {
+        return usersRepository.findAll();
+    }
+
+    public void deleteUser(Long id) {
+        usersRepository.deleteById(id);
+    }
+
+
 
 }
